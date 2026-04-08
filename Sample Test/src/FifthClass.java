555c3957d7b27f2740ddd9feecf5a9c287a191ed
ganapathiraja2801 <prganapathiraja@gmail.com>
2026-04-08 22:34:51 +0530

    Update FifthClass.java

--- Sample Test/src/FifthClass.java
+++ Sample Test/src/FifthClass.java
@@ -5,6 +5,15 @@ public class FifthClass {
 		// TODO Auto-generated method stub
 		//Fifth Class Daw
 
+		public int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+
 	}
 
 }
